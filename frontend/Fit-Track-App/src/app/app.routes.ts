1548c15db47f6cbf8d839c4f1ef83f0b65880659
Eliezer Perl <eliezerrules@gmail.com>
2025-07-28 15:36:22 +0300

    dashboard progress start

--- frontend/Fit-Track-App/src/app/app.routes.ts
+++ frontend/Fit-Track-App/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
 import { Dashboard } from './features/dashboard/dashboard';
 import { NotFound } from './features/not-found/not-found';
+import { Progress } from './features/dashboard/progress/progress';
+import { DashboardHome } from './features/dashboard/dashboard-home/dashboard-home';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -12,6 +14,24 @@ export const routes: Routes = [
   { path: 'register', component: Register },
   { path: 'food', component: Food },
   { path: 'fitness', component: Fitness },
-  { path: 'dashboard', component: Dashboard },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    children: [
+      {
+        path: 'progress',
+        component: Progress,
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home', // optional landing screen inside dashboard
+      },
+      {
+        path: 'home',
+        component: DashboardHome, // the current dashboard home content
+      },
+    ],
+  },
   { path: '**', component: NotFound },
 ];
---
+++ frontend/Fit-Track-App/src/app/features/dashboard/dashboard-home/dashboard-home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard-home',
+  imports: [RouterModule],
+  templateUrl: './dashboard-home.html',
+  styleUrl: './dashboard-home.css',
+})
+export class DashboardHome {}
